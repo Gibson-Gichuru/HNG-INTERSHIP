d85b3752ceb568e965cfbed585fca7ac3d1ac237
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-11-03 12:16:56 +0300

    Arithmetic endpoint testing

--- tests/test_app_main_endpoint.py
+++ tests/test_app_main_endpoint.py
@@ -1,5 +1,6 @@
 from tests import BaseTestConfig
 
+from flask import json
 
 class TestAppEndPoints(BaseTestConfig):
 
@@ -8,7 +9,6 @@ class TestAppEndPoints(BaseTestConfig):
         super().setUp()
 
         self.client = self.app.test_client()
-
 
 
     def test_app_main_endpoint(self):
@@ -21,3 +21,38 @@ class TestAppEndPoints(BaseTestConfig):
             "age":22,
             "bio":"am a fullstack webdeveloper with two years work experience"
         })
+
+    def make_post_request(self, operation_type, num1, num2):
+
+        data = json.dumps({"operation_type":operation_type,"x":num1,"y":num2})
+
+        headers={"content-type": "application/json"}
+
+        res = self.client.post("/api",headers=headers,data=data)
+
+        return res
+
+
+    def test_arithmetic_endpoint_supports_addition(self):
+
+        response = self.make_post_request("addition",2,3)
+
+        answ = response.get_json()
+
+        self.assertEqual(answ.get("result"), 5)
+
+    def test_arithmetic_endpoint_supports_subtraction(self):
+
+        response = self.make_post_request("subtraction",2,3)
+
+        answ = response.get_json()
+
+        self.assertEqual(answ.get("result"), -1)
+
+    def test_aithmetic_endpoint_supports_multiplication(self):
+
+        response = self.make_post_request("multiplication", 2,3)
+
+        answ = response.get_json()
+
+        self.assertEqual(answ.get("result"), 6)
